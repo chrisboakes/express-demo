d9dc048a8a98d7874e64c26b40b69af4b4d5785c
Chris Boakes <chris@chrisboakes.com>
2020-05-19 12:21:04 +0100

    Added CSS build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
+import path from "path";
+import postcss from "rollup-plugin-postcss";
+
 export default {
 	input: 'src/main.js',
 	output: {
 		file: 'public/dist/bundle.js',
 		format: 'cjs'
-	}
+	},
+	plugins: [
+    postcss({
+		extract: path.resolve('public/dist/style.css'),
+		plugins: [
+			require('autoprefixer')
+		]
+    })
+  ]
 };
