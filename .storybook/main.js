826db028d691b211e116a5cef94f1a05e2ba515e
Chris Boakes <chris@chrisboakes.com>
2020-06-16 15:40:20 +0100

    Added Handlebars support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,16 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-a11y',
-	'@storybook/addon-docs'
-  ]
+	stories: ['../stories/**/*.stories.js'],
+	addons: [
+		'@storybook/addon-a11y',
+		'@storybook/addon-docs'
+	],
+	// Add handlebars support
+	webpackFinal: async (config, { configType }) => {
+		config.module.rules.push({
+			test: /\.handlebars|hbs$/,
+			loader: "handlebars-loader"
+		});
+
+		return config;
+	}
 };
