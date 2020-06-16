e856c48ea75d9742093fc982a1ef04e189d4992a
Chris Boakes <chris@chrisboakes.com>
2020-06-16 15:42:35 +0100

    Added handlebars loader to Navigation

--- stories/navigation.stories.js
+++ stories/navigation.stories.js
@@ -1,21 +1,14 @@
 import '../src/css/utils/reset.css';
 import '../src/css/utils/generic.css';
 import '../src/css/components/navigation.css';
+import Navigation from "../views/partials/navigation.handlebars";
 
 export default {
 	title: 'Navigation',
 };
 
-const markup = `<nav class="navigation">
-	<ul class="navigation__list">
-		<li class="navigation__item"><a href='/'>Home</a></li>
-		<li class="navigation__item"><a href='/politics'>Politics</a></li>
-		<li class="navigation__item"><a href='/news'>News</a></li>
-	</ul>
-</nav>`;
-
-export const navigation = () => markup;
+export const navigation = () => Navigation();
 
 export const navigationSelected = () => {
-	return markup.replace('navigation__item', 'navigation__item navigation__item--selected');
+	return Navigation().replace("navigation__item", "navigation__item navigation__item--selected");
 };
